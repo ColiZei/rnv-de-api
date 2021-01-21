073c3751330e4f9767c00936a749dd37e4ce4785
Sebastian Collazo-Quaas <mission@collazo.de>
2021-01-21 20:19:31 +0100

    Added custom error handler

--- app.js
+++ app.js
@@ -12,6 +12,11 @@ app.use((req, res, next) => {
   res.status(404).send({ error: '404 - Not found' });
 });
 
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.send({ error: err.message });
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Listen to port ${PORT}...`);
