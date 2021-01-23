0409688711e4e296aa74692ad5031e8d61423e4b
Sebastian Collazo-Quaas <mission@collazo.de>
2021-01-23 18:07:25 +0100

    Added mocha and chai for testing

---
+++ test/example.test.js
@@ -0,0 +1,9 @@
+// // const { expect } = chai;
+
+// const { expect } = require('chai');
+
+// describe('Test Description', function () {
+//   it('should work', function () {
+//     expect(1 + 1).to.be.equal(2);
+//   });
+// });
