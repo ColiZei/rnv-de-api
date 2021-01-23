d97c38aca73233ea54c13479329f6695fa360f62
Sebastian Collazo-Quaas <mission@collazo.de>
2021-01-24 00:58:57 +0100

    Added sequelize

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 
+const { sequelize } = require('./util/database');
 const locationRoutes = require('./routes/location');
 
 const app = express();
@@ -21,6 +22,13 @@ app.use((err, req, res, next) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Listen to port ${PORT}...`);
-});
+
+try {
+  sequelize.sync();
+  // sequelize.sync({ force: true });
+  app.listen(PORT, () => {
+    console.log(`Listen to port ${PORT}...`);
+  });
+} catch (err) {
+  throw err;
+}
